980540d06108c4812625a3afd3a8faf7506d8aff
Olegueyan <gsourtg2cer@mail.com>
2023-01-26 10:01:30 +0100

    Import Bootstrap

--- Controller/login.php
+++ Controller/login.php
@@ -3,6 +3,6 @@
 require_once("../Model/Manager/UserManager.php");
 
 $title = "GitLibrary : Log In";
-$action = '../Model/Action/login_action.php';
+$action = '../login_action.php';
 
 require_once('../Vue/login_vue.php');
