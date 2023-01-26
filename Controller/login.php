afde502ad7b3fddd4b1a2c57f921d7d4c216e518
Olegueyan <gsourtg2cer@mail.com>
2023-01-26 11:51:38 +0100

    font et css

--- Controller/login.php
+++ Controller/login.php
@@ -3,6 +3,6 @@
 require_once("../Model/Manager/UserManager.php");
 
 $title = "GitLibrary : Log In";
-$action = '../login_action.php';
+$action = 'login_action.php';
 
 require_once('../Vue/login_vue.php');
